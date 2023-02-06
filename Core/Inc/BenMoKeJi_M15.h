99903ff5727233924d1c8e0f5f0117af981ac0f5
JiayeyeChen <invention.island@gmail.com>
2023-02-06 15:10:22 +0800

    Update BenMoKeJi_M15.h

--- Core/Inc/BenMoKeJi_M15.h
+++ Core/Inc/BenMoKeJi_M15.h
@@ -1,7 +1,14 @@
 #ifndef BENMOKEJI_M15_H
 #define BENMOKEJI_M15_H
 
+#include "system_periphrals.h"
+#include "can_bus.h"
 
+typedef struct
+{
+	CAN_HandleTypeDef*    hcan;
+	
+}BENMOKEJI_M15_Handle;
 
 
 #endif
